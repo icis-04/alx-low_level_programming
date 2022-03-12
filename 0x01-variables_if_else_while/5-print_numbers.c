eb4adddd4a25b13a2806e539fcd8bf390a9122cd
icis-04 <chidexiwuaku@gmail.com>
2022-03-11 18:51:43 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int s = 0;
+while (s < 10)
+{
+	printf("%d", s);
+	s++;
+}
+printf("\n");
+return (0);
+}
