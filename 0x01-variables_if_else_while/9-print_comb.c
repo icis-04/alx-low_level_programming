ecde5b27c0d3279556c647308e895d69edc6a74f
icis-04 <chidexiwuaku@gmail.com>
2022-03-11 20:14:10 -0800

    Success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
- /**
-  * main - prints number cominations using putchar
-  *
-  * Return: Always 0 (Success)
-  */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
