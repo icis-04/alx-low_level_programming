f7931634f8e7cf2b8f2ac9dd42db62d9aed9bc0b
icis-04 <chidexiwuaku@gmail.com>
2022-03-11 16:23:06 -0800

    4- print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <std.io.h>
+/**
+ * main -prints the alphabet except c and a
+ *
+ * (Return:0  on succes;
+ */
+int main(void)
+{
+char c = 'a';
+while(c <= 'z')
+{
+	if (c != 'q' && c!='e')
+{
+	putchar(c);
+}
+c++;
+putchar('\n');
+return (0);
+}
+}
