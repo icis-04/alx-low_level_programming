e8e549bb619f59654c03a76dc1d9fc5c51f26a3c
icis-04 <chidexiwuaku@gmail.com>
2022-03-15 15:02:42 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- * _islower - checks if number is lowercase
+ * _islower - checks if number is lowercase.
  * Return: 0 or 1.
+ * c - return int.
  */
 int _islower(int c)
 {
 if ((c >= 'a') && (c <= 'z'))
 {
 return (1);
-} else 
+} 
+else { 
 return (0);
+}
 _putchar('\n');
 }
 
