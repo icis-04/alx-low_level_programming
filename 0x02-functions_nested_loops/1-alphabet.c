735235ce8bcb00d2d4f926e9cdc042761461ba75
icis-04 <chidexiwuaku@gmail.com>
2022-03-15 10:27:41 -0700

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints a - z
  *   
  * Return: Always 0.
  */
@@ -11,7 +11,7 @@ void print_alphabet(void)
 int c;
 for (c = 'a'; c <= 'z'; c++)
 {
-	_putchar(c);
+_putchar(c);
 }
 _putchar('\n');
 }
