1179edd276ea204b52328ca2722383545b91c62b
icis-04 <chidexiwuaku@gmail.com>
2022-03-16 09:09:06 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - print sum of numbers
+ * @c: input
+ * @v: input
+ * Return: sum of numbers or 0
+ */
+int add(int v, int c)
+{
+int result  = c + v;
+return result;
+}
+
