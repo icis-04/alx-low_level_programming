e5139e7b29b395f92fae4c23ae0b4ee72157ae09
icis-04 <chidexiwuaku@gmail.com>
2022-03-17 11:30:37 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * more_numbers - prints numbers from 1 to 14 10 times.
+ */
+void more_numbers(void)
+{
+int i = 0;
+int j;
+while (i < 10)
+{
+j = '0';
+while (j <= '14')
+{
+_putchar(j);
+j++;
+}
+i++;
+}
+}
+
