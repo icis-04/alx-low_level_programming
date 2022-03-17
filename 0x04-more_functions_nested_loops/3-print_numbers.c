5f39ee43afc8eeecbac06e5d856616f09b5ba57e
icis-04 <chidexiwuaku@gmail.com>
2022-03-17 10:55:46 -0700

    0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - prints from 0 to 9
+ */
+void print_numbers(void)
+{       
+int a;   
+for (a = '0'; a <= '9'; a++)
+{       
+putchar(a);
+}
+_putchar('\n');
+}
+
