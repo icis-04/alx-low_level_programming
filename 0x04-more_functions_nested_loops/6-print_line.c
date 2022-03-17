cda17daa4f93d431db187c8d794c8132c9b26e68
icis-04 <chidexiwuaku@gmail.com>
2022-03-17 15:25:31 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: input
+ */
+void print_line(int n)
+{
+int i = 0;
+if (n == 0 || n < 0)
+{
+_putchar('\n');
+}
+else
+{
+while (i < n)
+{
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
+}
