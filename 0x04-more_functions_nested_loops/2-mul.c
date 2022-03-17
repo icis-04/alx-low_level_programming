78bf87dd60f3ff2c78363cda8c08a8ab2b59d9cf
icis-04 <chidexiwuaku@gmail.com>
2022-03-17 10:20:14 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: input
+ * @b: input
+ * Return: return product of a and b
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
+
