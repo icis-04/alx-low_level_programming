20213d30404b5b0fbd2a3821be2717b04487c004
icis-04 <chidexiwuaku@gmail.com>
2022-03-16 20:21:49 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: sum of multiples
+ */
+int main(void)
+{
+int c;
+int result = 0;
+for (c = 0; c < 1024; c++)
+{
+if ((c % 3 == 0) || (c % 5 == 0))
+{
+result = result + c;
+}
+return (result)
