60b4d20f7560931ff5f6006bb220b977bc34fdfa
icis-04 <chidexiwuaku@gmail.com>
2022-03-17 20:43:04 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * fizzbuzz - Entry point
+ * main - Entry point
+ * Return: 0 Always
  */
-void fizzbuzz(void)
+int main(void)
 {
 int i;
-for (i = 0; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 !=0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else if (i % 15 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
 }
 else
 {
-printf("%d ", i);
+printf(" %d", i);
 }
 }
+printf("\n");
+return (0);
 }
