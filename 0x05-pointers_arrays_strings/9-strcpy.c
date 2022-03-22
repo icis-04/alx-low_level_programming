5c497e8b8d0816202cbef68609aea149abe64e2f
icis-04 <chidexiwuaku@gmail.com>
2022-03-22 15:37:16 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - Entry point
+ * @dest: first input
+ * @src: second input
+ * Return: Always 0. 
+ */
+char *_strcpy(char *dest, char *src)
+{
+char s1[98];
+char *ptr;
+ptr _strcpy(s1, "First, solve the problem. Then, write the code\n");
+printf(%s, s1);
+printf(%s, ptr);
+return (0);
+}
+
