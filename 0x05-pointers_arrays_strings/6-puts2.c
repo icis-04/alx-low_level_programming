c66979d2384d57e56643238bb17298c55dbbceaa
icis-04 <chidexiwuaku@gmail.com>
2022-03-22 04:12:35 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * @str: input
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
+}
+
+
+
+
