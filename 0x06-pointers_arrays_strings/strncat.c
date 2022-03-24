6bc10bd0258ba03a8c99ca5d4fc96d3f6cef7514
icis-04 <chidexiwuaku@gmail.com>
2022-03-24 12:41:59 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - Entry point
+ * @dest: first input
+ * @src: second input
+ * @n: third input
+ * Return: concatenate src to dest with n bytes
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < n)
+{
+dest[j + i] = src[j];
+}
+else
+{
+dest[j + i] = '\0';
+}
+return (dest);
+}
+}
