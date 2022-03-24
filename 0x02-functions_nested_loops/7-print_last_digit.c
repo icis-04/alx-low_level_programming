e7ac8a5134423ed2ba18d32c36fc72aff0f4a74e
icis-04 <chidexiwuaku@gmail.com>
2022-03-23 23:48:39 -0700

    last digit fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,21 +8,14 @@
 int print_last_digit(int c)
 {
 int last_number, result;
-if (c > 0)
+if (c >= 0)
 {
 last_number = c % 10;
-_putchar(last_number);
 }
 else if (c < 0)
 {
 result = c * -1;
 last_number = result % 10;
-_putchar(last_number);
-}
-else
-{
-last_number = 0;
-_putchar(last_number);
 }
 _putchar('0' + last_number);
 return (last_number);
