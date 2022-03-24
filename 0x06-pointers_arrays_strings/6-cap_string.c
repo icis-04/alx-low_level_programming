c1ed11b8c38cb84183b37543681f0df0a8a459d4
icis-04 <chidexiwuaku@gmail.com>
2022-03-24 16:54:47 -0700

    tech bros

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalize characterize
+ * @s: string
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+int i = 0;
+int j;
+char spec[] = {' ', '\t', '\n', ',', ';', '.', '!', '"', '(', ')', '{', '}'};
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] -= 32;
+}
+while (s[i] != '\0')
+{
+for (j = 0; spec[j] != '\0'; j++)
+{
+if (s[i - 1] == spec[j] && s[i] <= 122 && s[i] >= 97)
+{
+s[i] = s[i] - 32;
+}
+i++;
+}
+return (s);
+}
+
