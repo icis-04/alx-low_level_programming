b31ac1399f7c38994d76fc4179bae3ebfad060d3
icis-04 <chidexiwuaku@gmail.com>
2022-03-24 17:21:16 -0700

    done for the day

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - Encode strings
+ * @a: array
+ * Return: a
+ */
+char *leet(char *a);
+char s[] = "aAeEoOtTlL";
+char p[] = "4433007711";
+int i = 0;
+int j = 0;
+while (a[i] != '\0')
+{
+for (j = 0; s[i] != '\0' && a[i] != s[j]; j++)
+;
+if (j < 10)
+{
+a[i] = p[j];
+}
+i++;
+}
+return (a);
+}
+
