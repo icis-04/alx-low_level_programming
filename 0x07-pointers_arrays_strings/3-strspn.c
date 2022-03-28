0df19fa4a14c79608f33a5818285d38fa8594ed0
icis-04 <chidexiwuaku@gmail.com>
2022-03-28 15:33:31 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * strspn - counts the number of bytes of astring in another string.
+ * @s: initial string.
+ * @accept: bytes you checking for.
+ * Return: number of integers that tally.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[b]; j++)
+{
+if (s[a] == accept[b])
+{
+break;
+}
+if (s[a] != accept[b])
+break;
+}
+return (a);
+}
+
+
