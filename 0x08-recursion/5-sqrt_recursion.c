0c49925b4704a9cbdcc2e50d8510e2346b8b3f64
icis-04 <chidexiwuaku@gmail.com>
2022-03-31 13:02:37 -0700

    sqare root things

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,30 @@
 #include "main.h"
 /**
- * _sqrt_recursion - finds square root of a number 
- * @n: input 
- * Return: returns square root of the input
+ * _square - finds square of a number 
+ * @n: input
+ * @i: input 
+ * Return: returns square of the input
  */
-int _sqrt_recursion(int n)
-{
-int i = 0;
-i++;
-if  (i <= n)
+int _square(int i, int n)
 {
 if (i * i == n)
 {
 return (i);
 }
-else
+else if (i > n / 2)
 {
 return (-1);
 }
+return (_square(i + 1, n));
 }
-return (0);
+
+/**
+ * _sqrt_recursion - finds square root of a number
+ * @n: input
+ * Return: returns square root of the input
+ */
+int _sqrt_recursion(int n)
+{
+return (_square(1, n));
 }
+
