dd03cc2ed788a261274a8ff6d8b6e026a4f2119c
icis-04 <chidexiwuaku@gmail.com>
2022-04-05 17:39:15 -0700

    holberton duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns pointer to the pointer of the duplicate 
+ * @str: input string to be duplicated
+ * Return: returns duplicated string
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int j;
+int i;
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i]; i++)
+{
+;
+}
+i++;
+dup = malloc(sizeof(char) * i);
+for (j = 0; j < i; j++)
+{
+dup[j] = str[j];
+if (dup == NULL)
+{
+return (NULL);
+}
+}
+return (dup);
+}
