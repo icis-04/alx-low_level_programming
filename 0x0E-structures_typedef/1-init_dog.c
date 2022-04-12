da881b0614e5b6e1c4ad60985fbe519f5c12e679
icis-04 <chidexiwuaku@gmail.com>
2022-04-11 17:05:50 -0700

    poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * init_dog - initializes a dog structure
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
+/**
+ * dog - new structure
+ * @name: character
+ * @age: character
+ * @owner: character
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
