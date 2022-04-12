3e1a477fcbc896dff144b747ad57d4b3e5eec3a9
icis-04 <chidexiwuaku@gmail.com>
2022-04-11 18:00:23 -0700

    e choke

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,13 +9,8 @@
  * @owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-if (d != '\0')
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
-}
-else
-{
-return;
 }
