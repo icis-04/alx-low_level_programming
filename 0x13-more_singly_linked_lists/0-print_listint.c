5878cc536bcbdf170177244ba0aca736315e1f2a
icis-04 <chidexiwuaku@gmail.com>
2022-04-26 10:21:39 -0700

    the fixed commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-unsigned int i;
+unsigned int i = 0;
 while (h != NULL)
 {
 if (h->n)
 {
-printf("%d", h->n);
+printf("%d\n", h->n);
 }
 i++;
 h = h->next;
